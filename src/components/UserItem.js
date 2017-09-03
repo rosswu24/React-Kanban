1cc23269afed197b6e0f0ac7b5aa1af94baa82fd
rosswu24 <wuyawchia24@gmail.com>
2017-09-02 14:26:57 -1000

    no using any file named user*, using those for reference, just build folder structure

---
+++ src/components/UserItem.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const UserItem = ({name})=>{
+//  return (
+//    <li>
+//      {name}
+//    </li>
+//    )
+// }
+
+// export default UserItem;
---
+++ src/components/UserList.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import UserItem from '/UserItem.js'
+
+// const UserList =({users}) =>{
+//  return (
+//    <ul>
+//      {
+//        users.map(user=>{
+//          return(
+//            <UserItem name={user}/>
+//            )
+//        })
+//      }
+//    </ul>
+//    )
+// }
+
+
+// export default UserList;
