df9173fba7e94fc9c522c6c02b6f0c2c27115bb9
rosswu24 <wuyawchia24@gmail.com>
2017-09-04 13:35:31 -1000

    added status and priority, also added comment to add for later future use

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 // import logo from '.././logo.svg';
-import NewCard from '../NewCard/'
+import NewCard from '../NewCard/';
+import queueComponents from '../../components/Queue.js';
 import {connect} from 'react-redux';
 import {addCard} from '../../actions/Cards.js'; // grab from the actions/Card, function
 import './App.css';
+
 
 class App extends Component{
 	render(){
@@ -11,10 +13,41 @@ class App extends Component{
 			<div>
 				<h1>hello world</h1>
 
-				<NewCard />
+				<section id = 'queue'>
+					<h2> Queue </h2>
+						<NewCard 
+							newCardTitle= {this.props.title}
+							newCardPriority = {this.props.priority}
+							newCardCreated={this.props.created}
+							newCardAssigend={this.props.assigned}
+						/>
+				</section>
+
+				<section id = 'inProgress'>
+					<h2>In Progress</h2>
+						Progess page
+				</section>
+
+				<section id = 'done'>
+					<h2>Done</h2>
+						Done
+				</section>
+
+
+
+
 				</div>
 			)
 	}
 
 }
-export default App;
+
+const mapStateToProps = (state) =>{
+	return {
+		title: state.title,
+		priority: state.priority,
+		created: state.created
+	}
+}
+const ConnectedApp = connect()(App)
+export default ConnectedApp;
