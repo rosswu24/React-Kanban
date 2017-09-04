ac589e4824d74576b90cb8b28684d20c3c5df727
rosswu24 <wuyawchia24@gmail.com>
2017-09-04 13:33:09 -1000

    just changed to card:card

---
+++ src/actions/Users.js
@@ -0,0 +1,11 @@
+// export const ADD_USER = 'ADD_USER';
+// export const REMOVE_USER = 'REMOVE_USER';
+// export const LOAD_USERS = 'LOAD_USERS';
+
+// export const addUser = (USER) => {
+//  return {
+//    type: ADD_USER,
+//    user:user
+//  };
+//  }
+
